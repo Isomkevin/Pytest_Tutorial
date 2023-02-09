0ecf1908aefc56fd16ac4f06fae00eb171e87d12
Kevin Isom <47275702+Isomkevin@users.noreply.github.com>
2023-02-09 09:02:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class ShoppingCart:
+  def __init__(self) -> None:
+    pass
+  
+  def add(self, item: str):
+    pass
+
+  def size(self) -> int:
+    pass
+
+  def get_items(self) -> List(str):
+    pass
+
+  def get_total_prize(self, price_map):
+    pass
